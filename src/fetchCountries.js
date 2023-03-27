fd381c5f653dc0420a62c265b433f5d9ca38074d
VitalyOvsiyenko <ovsiyenko80@gmail.com>
2023-03-27 12:44:37 +0300

    update index.js, fetchCountries.js, styles.css

---
+++ src/fetchCountries.js
@@ -0,0 +1,7 @@
+const BASE_URL = 'https://restcountries.com/v3.1/name/';
+
+export function fetchCountries(name) {
+  return fetch(`${BASE_URL}${name}?fields=name,capital,population,flags,languages`)
+    .then(response => response.json())
+    .catch(error => console.log(error))
+}
